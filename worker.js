8b20335e8d9a42719360be4995fc70fcd5b04b03
GameDev46 <76485006+GameDev46@users.noreply.github.com>
2025-10-26 15:41:57 +0000

    Refactor worker.js by cleaning up code and comments
    - Removed commented-out code and author information from the top of the file
    - Cleaned up whitespace and improved distance calculation methods

--- worker.js
+++ worker.js
@@ -1,34 +1,11 @@
-/*
-
- _____                         ______                 ___   ____ 
-|  __ \                        |  _  \               /   | / ___|
-| |  \/  __ _  _ __ ___    ___ | | | |  ___ __   __ / /| |/ /___ 
-| | __  / _` || '_ ` _ \  / _ \| | | | / _ \\ \ / // /_| || ___ \
-| |_\ \| (_| || | | | | ||  __/| |/ / |  __/ \ V / \___  || \_/ |
- \____/ \__,_||_| |_| |_| \___||___/   \___|  \_/      |_/\_____/
-
-
-*/
-
-/* 
-	AUTHOR: GameDev46
-
-	Youtube: https://www.youtube.com/@gamedev46
-	Github: https://github.com/GameDev46
-*/
-
 // Workers to speed up rendering all the pixels
-
 let gameobjects = []
 
 let pixelSize = 10;
 
 let worldLights = [];
-
 let lightWorld = true;
-
 let maxBounces = 6;
-
 let worldAmbience = 0.3;
 
 let camera = {
@@ -46,7 +23,6 @@ let window = {
 }
 
 self.onmessage = (msg) => {
-
 	gameobjects = msg.data.gameobjects;
 	pixelSize = msg.data.pixelSize;
 	worldLights = msg.data.worldLights
@@ -161,17 +137,13 @@ function getPixelColour(x, y) {
 			else {
 
 				// No more reflections - ready to render pixel
-
 				if (bounces == 0) {
 					// No reflection
-
 					savedColour = dist[0].colour;
 				}
 				else {
 					// Combine all reflections
-
 					let lastObjectColour = lightPixel(currentPos, dist[0].colour, x / pixelSize, y / pixelSize);
-
 					savedColour = [lastObjectColour.r, lastObjectColour.g, lastObjectColour.b];
 
 					// Loop backward through all hit surfaces and compile a final colour
@@ -184,7 +156,6 @@ function getPixelColour(x, y) {
 					}
 
 					currentPos = savedReflectionPos;
-
 				}
 
 				// Get the colour of the pixel based on lighting
@@ -215,11 +186,9 @@ function getPixelColour(x, y) {
 
 		// Loop backward through all hit surfaces and compile a final colour
 		for (let i = savedColours.length - 1; i >= 0; i--) {
-
 			savedColour[0] = lerp(savedColours[i][0], savedColour[0], savedReflectivity[i]);
 			savedColour[1] = lerp(savedColours[i][1], savedColour[1], savedReflectivity[i]);
 			savedColour[2] = lerp(savedColours[i][2], savedColour[2], savedReflectivity[i]);
-
 		}
 
 		// Get the colour of the pixel based on lighting
@@ -233,7 +202,6 @@ function getPixelColour(x, y) {
 		}
 
 		return lightingData;
-
 	}
 
 	// return sky colour as no object found
@@ -254,18 +222,15 @@ function noise(x, y) {
 
 function getLargestSafeDistance(x, y, z) {
 	let smallestDistance = Infinity;
-
 	let closestGameObject = null;
 
 	for (let i = 0; i < gameobjects.length; i++) {
-
 		if (gameobjects[i].model == "sphere") {
 			if (getDistance(gameobjects[i].position, { x: x, y: y, z: z }) - gameobjects[i].scale.r < smallestDistance) {
 				smallestDistance = getDistance(gameobjects[i].position, { x: x, y: y, z: z }) - gameobjects[i].scale.r;
 
 				closestGameObject = gameobjects[i];
 			}
-
 		}
 		else if (gameobjects[i].model == "box") {
 			if (getDistanceFromSquare({ x: x, y: y, z: z }, gameobjects[i].position, gameobjects[i].scale, gameobjects[i].bump, gameobjects[i].tiles) < smallestDistance) {
@@ -302,57 +267,40 @@ function getLargestSafeDistance(x, y, z) {
 }
 
 function getDistance(position1, position2) {
-
 	let posDif = { x: position2.x - position1.x, y: position2.y - position1.y, z: position2.z - position1.z }
-
-	let distance2d = (posDif.x * posDif.x) + (posDif.y * posDif.y)
-
-	let distance3d = Math.sqrt(distance2d + (posDif.z * posDif.z))
-
+	let distance3d = Math.sqrt((posDif.x * posDif.x) + (posDif.y * posDif.y) + (posDif.z * posDif.z))
 	return distance3d;
 }
 
 function getDistanceFromSquare(pointPos, objectPos, size, bumpPower, tiles) {
-
 	let offset = [Math.abs(pointPos.x - objectPos.x) - size.x, Math.abs(pointPos.y - objectPos.y) - size.y, Math.abs(pointPos.z - objectPos.z) - size.z];
-
 	let unsignedDist = length([Math.max(offset[0], 0), Math.max(offset[1], 0), Math.max(offset[2], 0)]);
-
 	return unsignedDist;
 }
 
 function distFromCubeWithSphereHole(pointPos, objectPos, size, bumpPower, tiles) {
 	let sphereDist = getDistance(objectPos, pointPos) - size.r;
 	let cubeDist = getDistanceFromSquare(pointPos, objectPos, size, bumpPower, tiles)
-
 	return Math.max(-sphereDist, cubeDist);
 }
 
 function distFromSphereWithFlatSides(pointPos, objectPos, size, bumpPower, tiles) {
 	let sphereDist = getDistance(objectPos, pointPos) - size.r;
 	let cubeDist = getDistanceFromSquare(pointPos, objectPos, size, bumpPower, tiles)
-
 	return Math.max(sphereDist, cubeDist);
 }
 
 function getDistanceFromGyroid(pointPos, objectPos, size, bumpPower, tiles) {
-
 	//let gyroid = dot([Math.sin(pointPos.x * size.x), Math.sin(pointPos.y * size.y), Math.sin(pointPos.z * size.z)], [Math.cos(pointPos.z * size.z), Math.cos(pointPos.x * size.x), Math.cos(pointPos.y * size.y)])
-
 	let repeatAmount = 100
-
 	let gyroid2 = distFromCubeWithSphereHole({ x: pointPos.x % repeatAmount, y: pointPos.y % repeatAmount, z: pointPos.z % repeatAmount }, objectPos, size, bumpPower, tiles);
-
 	return gyroid2;
 }
 
 dot = (a, b) => a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n);
 
 function length(posDif) {
-	let distance2d = (posDif[0] * posDif[0]) + (posDif[1] * posDif[1])
-
-	let distance3d = Math.sqrt(distance2d + (posDif[2] * posDif[2]))
-
+	let distance3d = Math.sqrt((posDif[0] * posDif[0]) + (posDif[1] * posDif[1]) + (posDif[2] * posDif[2]))
 	return distance3d;
 }
 
@@ -364,10 +312,9 @@ function lightPixel(worldPos, objectColour, pixelX, pixelY) {
 		b: objectColour[2],
 		isInShadow: false
 	}
-
-	if (!lightWorld) {
-		return calcColour;
-	}
+	
+	// No need to calculate lighting if world is set to unlit
+	if (!lightWorld) return calcColour;
 
 	for (let i = 0; i < worldLights.length; i++) {
 
@@ -376,17 +323,13 @@ function lightPixel(worldPos, objectColour, pixelX, pixelY) {
 		}
 
 		// Calculate direction to light source
-
 		let dir = [worldLights[i][0][0] - worldPos.x, worldLights[i][0][1] - worldPos.y, worldLights[i][0][2] - worldPos.z]
-
 		div = Math.abs(dir[0]) + Math.abs(dir[1]) + Math.abs(dir[2]);
-
 		let worldLightDirection = [dir[0] / div, dir[1] / div, dir[2] / div];
 
 		// Move ray to starting position
 
 		let startDist = 0.2;
-
 		let newPos = [worldPos.x + (startDist * worldLightDirection[0]), worldPos.y + (startDist * worldLightDirection[1]), worldPos.z + (startDist * worldLightDirection[2])]
 
 		let hasFinished = false;
@@ -412,7 +355,6 @@ function lightPixel(worldPos, objectColour, pixelX, pixelY) {
 				// Calculate pixel brightness
 
 				let lightDist = getDistance({ x: worldLights[i][0][0], y: worldLights[i][0][1], z: worldLights[i][0][2] }, camera);
-
 				let lightIntensity = worldLights[i][2] / (lightDist ^ 2);
 
 				calcColour.r *= worldLights[i][3][0] * Math.max(Math.min(1, lightIntensity), 0);
@@ -423,7 +365,6 @@ function lightPixel(worldPos, objectColour, pixelX, pixelY) {
 			}
 
 			newPos = [newPos[0] + (dist[1] * worldLightDirection[0]), newPos[1] + (dist[1] * worldLightDirection[1]), newPos[2] + (dist[1] * worldLightDirection[2])];
-
 		}
 
 	}
@@ -445,8 +386,6 @@ function lerp(start, end, amt) {
 }
 
 function renderViewport(x, y) {
-
 	let pixelColour = getPixelColour(x, y)
-
 	return pixelColour;
 }
